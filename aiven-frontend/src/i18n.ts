f28ad8652e038aa23787a90ddfcc8ac0efebc36f
Dominik Reinert <dominik.reinert.merzig@googlemail.com>
2021-08-23 14:47:09 +0200

    initialize the project using create react app | copy over commonly used dependencies

---
+++ aiven-frontend/src/i18n.ts
@@ -0,0 +1,10 @@
+import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
+
+export const resources = {};
+
+i18n.use(LanguageDetector).use(initReactI18next).init({
+  fallbackLng: "en",
+  resources,
+});
