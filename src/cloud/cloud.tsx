05ab34a7466ac26c7aa3596bdd9ce7e2a7f878c5
Dominik Reinert <dominik.reinert.merzig@googlemail.com>
2021-08-24 15:01:45 +0200

    add another list layout that is easy to understand

--- src/cloud/cloud.tsx
+++ src/cloud/cloud.tsx
@@ -1,28 +1,54 @@
 /**@jsx jsx */
 import { css, jsx } from "@emotion/react";
 import { Cloud } from "../store/cloud_store";
+import { useStyleContext } from "../style_context/use_style_context";
 
 interface CloudProps extends Cloud {}
 
 export function CloudComponent(props: CloudProps): JSX.Element {
   const { cloudName, cloudDescription, geoRegion, geoLatitude, geoLongitude } =
     props;
+  const styleContext = useStyleContext();
   return (
     <div
       css={css`
         label: cloud-component;
+
+        width: 70%;
+
+        background-color: ${styleContext.colors.cardBackground};
+
+        margin: auto auto 20px auto;
+        padding: 16px;
+
+        box-shadow: 1px 3px 3px 1px ${styleContext.shades.boxShadow};
+        border-radius: 20px;
+
+        display: flex;
+        align-items: center;
+
+        .description {
+          font-size: ${styleContext.sizes.font.subHeadline};
+          color: ${styleContext.colors.aivenDark};
+        }
+
+        .name {
+          margin-left: 4px;
+          font-size: ${styleContext.sizes.font.text};
+          color: ${styleContext.colors.aivenDark};
+        }
+
+        > * {
+          flex: 1 0 0;
+        }
       `}
     >
-      <div className="name">{cloudName}</div>
       <div className="description">{cloudDescription}</div>
-      <div className="location-wrapper">
-        <div className="region">{geoRegion}</div>
-        <div className="distance-wrapper">
-          <div className="distance">distance</div>
-          <div className="coordinates">
-            {geoLongitude} ||| {geoLatitude}
-          </div>
-        </div>
+      <div className="name">({cloudName})</div>
+      <div className="region">{geoRegion}</div>
+      <div className="distance">distance</div>
+      <div className="coordinates">
+        {geoLongitude} ||| {geoLatitude}
       </div>
     </div>
   );
--- src/pages/create_cloud_page.tsx
+++ src/pages/create_cloud_page.tsx
@@ -4,17 +4,28 @@ import { Cloud, cloudStore } from "../store/cloud_store";
 interface CreateCloudPageConfig {
   fallback: string;
   cloudNamePrefix: string;
+  cloudDescriptionFormatter?: (description: string) => string;
 }
 
 interface ExtendedProps {
   clouds?: Cloud[];
 }
 
+// Removes all the unnecessary information in the description
+const defaultCloudDescriptionFormatter = (description: string) => {
+  const splits: string[] = description.split(":");
+  return splits[splits.length - 1];
+};
+
 export function createCloudPage<P>(
   Component: (props: P & ExtendedProps) => JSX.Element,
   config: CreateCloudPageConfig
 ): (props: P) => JSX.Element {
-  const { fallback, cloudNamePrefix } = config;
+  const {
+    fallback,
+    cloudNamePrefix,
+    cloudDescriptionFormatter = defaultCloudDescriptionFormatter,
+  } = config;
   return (props) => {
     return (
       <React.Suspense fallback={fallback}>
@@ -22,6 +33,7 @@ export function createCloudPage<P>(
           Component={Component}
           componentProps={props}
           cloudNamePrefix={cloudNamePrefix}
+          cloudDescriptionFormatter={cloudDescriptionFormatter}
         />
       </React.Suspense>
     );
@@ -32,11 +44,20 @@ function SuspendingCloudPage<P>(props: {
   Component: (p: P) => JSX.Element;
   componentProps: P;
   cloudNamePrefix: string;
+  cloudDescriptionFormatter: (description: string) => string;
 }): JSX.Element {
-  const { Component, componentProps, cloudNamePrefix } = props;
+  const {
+    Component,
+    componentProps,
+    cloudNamePrefix,
+    cloudDescriptionFormatter,
+  } = props;
   const clouds: Cloud[] = cloudStore
     .getCurrentDataAdapted()
     .clouds.filter((cloud) => cloud.cloudName?.startsWith(cloudNamePrefix));
+  clouds.forEach((cloud) => {
+    cloud.cloudDescription = cloudDescriptionFormatter(cloud.cloudDescription);
+  });
 
   return <Component {...componentProps} clouds={clouds} />;
 }
