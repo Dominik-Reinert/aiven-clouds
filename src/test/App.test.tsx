2c52c16ef7894bcf47375d46c30950384da690c3
Dominik Reinert <dominik.reinert.merzig@googlemail.com>
2021-08-28 07:30:25 +0200

    add enzyme and shallow renderingi nthe tests

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,7 +1,11 @@
-import * as assert from 'power-assert';
-import React from 'react';
-import App from '../App';
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import Enzyme, { shallow } from "enzyme";
+import * as assert from "power-assert";
+import * as React from "react";
+import App from "../App";
 
-test('renders without error', () => {
-  assert.doesNotThrow(() => <App />)
+Enzyme.configure({ adapter: new Adapter() });
+
+test("renders without error", () => {
+  assert.doesNotThrow(() => shallow(<App />));
 });
--- src/test/selection_context.spec.tsx
+++ src/test/selection_context.spec.tsx
@@ -1,11 +1,15 @@
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import Enzyme, { shallow } from "enzyme";
 import * as assert from "power-assert";
 import React from "react";
 import { PositionInput } from "../selection_context/position_input";
 import { ProviderInput } from "../selection_context/provider_input";
 import { RegionInput } from "../selection_context/region_input";
 
+Enzyme.configure({ adapter: new Adapter() });
+
 [PositionInput, ProviderInput, RegionInput].forEach((Component) => {
   test(`renders ${Component.name} without error`, () => {
-    assert.doesNotThrow(() => <Component />);
+    assert.doesNotThrow(() => shallow(<Component />));
   });
 });
